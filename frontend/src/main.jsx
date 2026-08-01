2d847f557cfa05d575f29178058c3931e9a30f3c
Nadine Hocking Wells <nadinehocking@gmail.com>
2026-08-01 11:15:09 +0100

    Updated stylesheet and scss structure

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.scss'
+// import './index.scss'
+import './styles/main.scss'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
